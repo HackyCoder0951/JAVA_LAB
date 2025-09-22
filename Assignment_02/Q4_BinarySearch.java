98d88fba7758b56788e661045a635dce4a3a4985
hackycoder0951 <jignesh.vision.17@gmail.com>
2025-09-22 23:14:24 +0530

    Refactor Q4 Binary Search to accept user input for array elements and update input/output example for clarity

--- Assignment_02/Q4_BinarySearch.java
+++ Assignment_02/Q4_BinarySearch.java
@@ -3,7 +3,13 @@ import java.util.*;
 class Q4_BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {2, 4, 6, 8, 10, 12, 14};
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " sorted elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         System.out.print("Enter number to search: ");
         int key = sc.nextInt();
 
@@ -30,10 +36,13 @@ class Q4_BinarySearch {
 }
 
 /*  Input/Output Example:
-    Enter number to search: 10
-    Element found at index 4
-*/
-/*  Input/Output Example:
-    Enter number to search: 5
-    Element not found
+    Enter number of elements: 5
+    Enter 5 sorted elements:
+    10
+    20
+    30
+    40
+    50
+    Enter number to search: 30
+    Element found at index 2
 */
