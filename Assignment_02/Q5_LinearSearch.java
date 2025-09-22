26f94fd00926ea6b723a9cd0ee7a3ab1704929e8
hackycoder0951 <jignesh.vision.17@gmail.com>
2025-09-22 23:13:45 +0530

    Refactor Q5 Linear Search to accept user input for array elements and add input/output example for clarity

--- Assignment_02/Q5_LinearSearch.java
+++ Assignment_02/Q5_LinearSearch.java
@@ -3,7 +3,13 @@ import java.util.*;
 class Q5_LinearSearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {10, 20, 30, 40, 50};
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         System.out.print("Enter element to search: ");
         int key = sc.nextInt();
         boolean found = false;
@@ -21,3 +27,15 @@ class Q5_LinearSearch {
 
     }
 }
+
+/*  Input/Output Example:
+    Enter number of elements: 5
+    Enter 5 elements:
+    10
+    20
+    30
+    40
+    50
+    Enter element to search: 30
+    Element found at index 2
+*/
